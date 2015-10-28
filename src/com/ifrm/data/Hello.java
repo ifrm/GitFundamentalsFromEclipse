946bfae76e62e4cc81d8189c7fd66dba85e1db15
Frim Ionatan <ionatanf@fortech.ro>
2015-10-28 15:22:18 +0200

    Adding getter and setters for String field hello from class Hello

--- src/com/ifrm/data/Hello.java
+++ src/com/ifrm/data/Hello.java
@@ -1,6 +1,14 @@
 package com.ifrm.data;
 
 public class Hello {
+	
+	private String hello;
+	public String getHello() {
+		return hello;
+	}
+	public void setHello(String hello) {
+		this.hello = hello;
+	}
 	public static void main (String[] args){
 		System.out.println("Hello there tibi !");
 	}
