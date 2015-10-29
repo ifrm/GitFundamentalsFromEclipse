38c4073dbee23f4a0af8f9b2d4bd84575d28599f
Frim Ionatan <ionatanf@fortech.ro>
2015-10-29 08:40:48 +0200

    Working on this Hllo class. adding a new field

--- src/com/ifrm/data/Hello.java
+++ src/com/ifrm/data/Hello.java
@@ -3,6 +3,13 @@ package com.ifrm.data;
 public class Hello {
 	
 	private String hello;
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	public String getHello() {
 		return hello;
 	}
@@ -12,11 +19,8 @@ public class Hello {
 	public static void main (String[] args){
 		Hello hh=new Hello();
 		hh.setHello("Buna ziua !");
-		
-		
-		
-		
-		System.out.println(hh.getHello());
+		hh.setName("Zoro");
+		System.out.println(hh.getHello()+" "+hh.getName());
 	}
 
 }
